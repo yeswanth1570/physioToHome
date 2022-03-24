60bd0cf262f46266bf7eab835f09d8d48e8991bd
yeswanth <yeswanth15750@gmail.com>
2022-03-24 22:19:19 +0530

    menu bar added

--- physioToHome/src/app/app.module.ts
+++ physioToHome/src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule ,CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+import { IonicModule } from '@ionic/angular';
 @NgModule({
   declarations: [
     AppComponent
@@ -13,7 +14,8 @@ import { MatSliderModule } from '@angular/material/slider';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatSliderModule,
+    IonicModule
   ],
   providers: [],
   bootstrap: [AppComponent]
